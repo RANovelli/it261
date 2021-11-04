f326e951dfa8cf83b2e376246e13c166932fb755
Ryan A. Novelli <77367277+RANovelli@users.noreply.github.com>
2021-11-03 17:33:18 -0700

    Update extracredit.php

--- weeks/week5/extracredit.php
+++ weeks/week5/extracredit.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Currency 3 - Sticky Form</title>
+    <title>Extra Credit Currency Assignment</title>
     <link href="css/ecstyles.css" type="text/css" rel="stylesheet">
 </head>
 
